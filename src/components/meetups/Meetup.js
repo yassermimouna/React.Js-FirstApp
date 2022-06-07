11809462a633755edf4139cb4f86d7ea4685fa2e
yasser <yesser.mimouna20@gmail.com>
2022-06-07 23:45:04 +0100

    Meetups-List
    Meetups-List

---
+++ src/components/meetups/Meetup.js
@@ -0,0 +1,23 @@
+import Card from "../userInterface/Card";
+import classes from "./Meetup.module.css";
+
+function Meetup(props) {
+  return (
+    <li className={classes.item}>
+      <Card>
+        <div className={classes.image}>
+          <img src={props.image} alt={props.title} />
+        </div>
+        <div className={classes.content}>
+          <h3>{props.title}</h3>
+          <address>{props.address}</address>
+          <p>{props.description}</p>
+        </div>
+        <div className={classes.actions}>
+          <button>To Favorites</button>
+        </div>
+      </Card>
+    </li>
+  );
+}
+export default Meetup;
