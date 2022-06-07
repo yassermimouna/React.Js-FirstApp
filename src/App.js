aaf836bcfdaee79cc01df3c0b62ae3506963454e
yasser <yesser.mimouna20@gmail.com>
2022-06-07 22:40:18 +0100

    adding-header
    adding-header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import AllMeetUpsPage from "./pages/AllMeetups";
+import NewMeetupPage from "./pages/NewMeetup";
+import FavoritesPage from "./pages/Favorites";
+import MainNavigation from "./components/layout/MainNavigation";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <MainNavigation />
+    <Routes>
+      <Route path="/" element={<AllMeetUpsPage />} />
+      <Route path="/newmeetup" element={<NewMeetupPage />} />
+      <Route path="/favorites" element={<FavoritesPage />} />
+    </Routes>
     </div>
   );
 }
